1fb95e87c28fbc0056209d5c2ccc198f69920b38
Abhishek-2502 <abhishek25022004@gmail.com>
2025-04-19 00:41:57 +0530

    include render

--- src/components/PredictionForm.jsx
+++ src/components/PredictionForm.jsx
@@ -25,7 +25,8 @@ const PredictionForm = () => {
     setResult(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://sellerscope.onrender.com/predict', {
+      // const response = await fetch('http://127.0.0.1:8000/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
